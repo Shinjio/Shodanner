7885fcd37f86cc46da6905cf86fa8e21d8255f3a
stoicboomer <stoicboomer@gmail.com>
2020-12-03 16:39:27 +0100

    fixed dns.py

--- src/dns.py
+++ src/dns.py
@@ -7,23 +7,30 @@ class DNS:
 
     #https://api.shodan.io/dns/domain/
     #Get all the subdomains for the given domain.
-    def domain(d):
+    def domain(self, d):
         r = json.loads(requests.get("https://api.shodan.io/dns/domain/{}".format(d),
-                       params = {"key" : token}).text)
-        return r["subdomains"]
+                       params = {"key" : self.token}).text)
+        try: 
+            return r["subdomains"]
+        except KeyError:
+            return []
 
     #https://api.shodan.io/dns/resolve
     #look up the IP address for the provided host.
-    def resolve(selfm host):
-        r = json.loads(requests.get("https://api.shodan.io/dns/resolve", 
-                       params = {"key" : self.token}).text)
-        return r
+    def resolve(self, host):
+        r = json.loads(requests.get("https://api.shodan.io/dns/resolve",
+                       params = {"hostnames" : host, "key" : self.token}).text)
+        return list(r.values())
     
     #https://api.shodan.io/dns/reverse
-    #Look up the hostnames that has been defined for the given IP addresses.
+    #Look up the hostnames that have been defined for the given IP addresses.
     def reverse(self, ip):
         r = json.loads(requests.get("https://api.shodan.io/dns/reverse",
                        params = {"ips" : ip, "key" : self.token}).text)
-        return r[list(r.keys())[0]]
+
+        #the api returns a dict with the ip as key and a list.. ugly but it works
+        r = [ d for i in r.values() for d in i] 
+        return r
+
 
     #TODO: subdomain fuzzing
