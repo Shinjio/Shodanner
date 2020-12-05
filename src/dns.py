573baff877e5f74f5eca8672d2f3225f71ead264
stoicboomer <stoicboomer@gmail.com>
2020-12-05 18:20:42 +0100

    interactive.py partial input handling and other:
    Added and improved the interactive.py Intertactive methods, now it can
    handle empty command without crashing the program.
    
    Important:
        - I edited dns.py and shodanner.py for other error handling.
    
    There is still a lot to do:
        - Handle errors on the Host object
    
        - Let the Host object use the exploits functions, otherwise
          the user has to switch between the different modes and that is
          pretty anoyying
    
        - IDEA:
            save into a list the devices the user finds by calling quickSearch,
            so he can recall these devices without the need of using the api again
            a.k.a wasting credits, and make this list movable between the Interactive Cmd
            and the Host Cmd, this will give the user more freedom. Also when printing
            the list of devices show the index so he can use this index as an argument
            for functions like info, reverse and others. (mouse-free navigation)
    
    Notes:
        - I added some comments on interactive.py go check them out
          the #DONE s are the function i suppose are now Exceptions-free but
          double checking doesn't hurt
    
    Tha's it for today, i'll get a break now

--- src/dns.py
+++ src/dns.py
@@ -8,6 +8,7 @@ class DNS:
     def domain(self, d):
     #https://api.shodan.io/dns/domain/
     #Get all the subdomains for the given domain.
+        
         r = json.loads(requests.get("https://api.shodan.io/dns/domain/{}".format(d),
                        params = {"key" : self.token}).text)
         try: 
@@ -25,12 +26,16 @@ class DNS:
     def reverse(self, ip):
     #https://api.shodan.io/dns/reverse
     #Look up the hostnames that have been defined for the given IP addresses.
+
         r = json.loads(requests.get("https://api.shodan.io/dns/reverse",
                        params = {"ips" : ip, "key" : self.token}).text)
-
         #the api returns a dict with the ip as key and a list.. ugly but it works
         try:
-            r = [ d for i in r.values() for d in i] 
-            return r
-        except:
-            return []
+            if r["error"]:
+                return r["error"]
+        except KeyError:
+            try:
+                r = [ d for i in r.values() for d in i] 
+                return r
+            except:
+                return []
--- src/interactive.py
+++ src/interactive.py
@@ -1,3 +1,4 @@
+import shodan
 import readline
 import colorama
 import sys
@@ -12,13 +13,19 @@ class Host(Cmd):
         super(Host, self).__init__()
         self.host = host
 
+    def emptyline(self):
+        pass
+
     def do_get(self, args):
         """Get an attribute from the host object (see the wiki for information about the HostParser object.)"""
-        if args == "vulns":
-            for i in self.host.vulns:
-                print(i.name + ": " + str(i.cvss) + " - " + i.references[0])
+        if args:
+            if args == "vulns":
+                for i in self.host.vulns:
+                    print(i.name + ": " + str(i.cvss) + " - " + i.references[0])
+            else:
+                print(getattr(self.host, args))
         else:
-            print(getattr(self.host, args))
+            print("please insert a valid attribute")
 
     def do_list(self, args):
         """List HostParser object's attributes (the ip you just entered).\nthe data attribute produces HUGE output"""
@@ -29,71 +36,160 @@ class Host(Cmd):
         """Exit"""
         return True
 
+""" NOTES 05/12/2020 Alex:
+        i wrote some error handling, the error messages should be changed
+        with something like a print of the syntax of the command or other,
+        i'm not good at formatting either, must improve for later commits.
+"""
+
 class Interactive(Cmd):
     def __init__(self):
         super(Interactive, self).__init__()
         self.shodanner = Shodanner("config.json");
 
+    def emptyline(self):
+        pass
+
     def do_quickSearch(self, query):
         """Do a scan with with your custom query, returns 25 ips"""
-        for i in self.shodanner.quickSearch(query):
-            print(i)
-
+        if query: 
+            for i in self.shodanner.quickSearch(query):
+                print(i)
+        else:
+            print("please enter a valid shodan query")
+    
+    #DONE
     def do_exploitdb(self, query):
         """Look for an exploit on exploitDB."""
-        print(self.shodanner.exploit.searchExploitDB(query))
+        if query:
+            for e in self.shodanner.exploit.searchExploitDB(query):
+                print(e)
+        else:
+            print("please enter an exploit name")
 
-    def do_cvedetails(self, query):
+    #DONE
+    def do_cvedetails(self, cve):
         """Look for info about a CVE on CVEDetails."""
-        print(self.shodanner.exploit.searchCVEDetails(query))
+        if cve.upper().startswith("CVE-"):
+            for e in self.shodanner.exploit.searchCVEDetails(cve):
+                print(e)
+        else:
+            print("please insert a valid cve name")
 
+    
+    #ALMOST DONE, there are probably other exception i didn't check in the Host cmd
     def do_info(self, args):
         """Return information about a specific IP"""
-        os.system('clear')
-        host = self.shodanner.host(args)
-        print(colorama.Fore.YELLOW + "You can now access each attribute by simply typing \"get\" followed by it's name, type \"list\" to see the attributes\n" + colorama.Fore.WHITE)
+        if args:
+            host = self.shodanner.host(args)
+            if not host:
+                print("coulndn't find the host ...maybe invalid ip?")
+                return
 
-        prompt = Host(host)
-        prompt.prompt =   "[" + colorama.Fore.RED + f"{host.ip}" + colorama.Fore.WHITE+ "] " + "⪢ " 
-        prompt.cmdloop()
+            os.system('clear')
+            print(colorama.Fore.YELLOW + "You can now access each attribute by simply typing \"get\" followed by it's name, type \"list\" to see the attributes\n" + colorama.Fore.WHITE)
 
+            prompt = Host(host)
+            prompt.prompt =   "[" + colorama.Fore.RED + f"{host.ip}" + colorama.Fore.WHITE+ "] " + "⪢ " 
+            prompt.cmdloop()
+        else:
+            print("please insert a valid ip")
+    
+    #DONE
     def do_myip(self, args):
         """Returns your IP address."""
         print(self.shodanner.myip())
 
+    #DONE
     def do_profile(self,args):
         """Returns information about your profile."""
-        print(self.shodanner.profile())
+        p = self.shodanner.profile()
+        #TOFIX, i seriously suck at formatting
+        print("""
+        Usage limits:
+            scan_credits: {}
+            query_credits: {}
+            monitored_ips: {}
 
+        scan_credis: {}
+        query_credits: {}
+        unlocked_left: {}
+        plan: {}\n""".format(p["usage_limits"]["scan_credits"], p["usage_limits"]["query_credits"], 
+                           p["usage_limits"]["monitored_ips"], p["scan_credits"], 
+                           p["query_credits"], p["unlocked_left"], p["plan"]))
+              
+
+    #DONE kinda, you should check if the ip is valid, apparently shodan will try
+    #to scan an invalid ip endinding up wasting the credit, bruh.
+    #Gosh, i finished my redbull.
     def do_scan(self, args):
         """Crawl an IP address, might take a while."""
-        print(self.shodanner.scan(args)) 
-
+        if args:
+            s = self.shodanner.scan(args)
+            try:
+                print("count: {}\nid: {}\ncredits_left: {}".format(s["count"], 
+                                                                   s["id"], s["credits_left"]))
+            except KeyError:
+                print(s["error"] + " ...maybe invalid ip?")
+    
+    #DONE
     def do_scanStatus(self, args):
         """Check the process of a previously submitted scan request.\n
-        Possible values for the status are: SUBMITTING, QUEQUE, PROCESSING, DONE
+        Possible values for the status are: SUBMITTING, QUEQUE, PROCESSING, DONE, NOT_FOUND
         """
-        print(self.shodanner.scanStatus(args))
+        if args:
+            print(self.shodanner.scanStatus(args))
+        else:
+            print("please insert a valid scan id")
 
+    #DONE
     def do_reverse(self,args):
         """Look up the hostnames that have been defined for the given IP addresses."""
-        print(self.shodanner.dns.domain(args))
-
+        dns = self.shodanner.dns.reverse(args)
+        if type(dns) == list:
+            for d in dns:
+                print(d)
+        else:
+            print(dns)
+    
+    #DONE
     def do_domain(self,args):
         """Get all the subdomains for a given domain"""
-        print(self.shodanner.dns.domain(args))
+        if args:
+            sub = self.shodanner.dns.domain(args)
+            if sub:
+                for s in sub:
+                    print(s)
+            else:
+                print("No subdomains found.")
+        else:
+            print("please insert a domain name")
 
+    #DONE
     def do_resolve(self, args):
         """Look up the IP address provided"""
-        print(self.shodanner.dns.resolve(args))
-
+        if args:
+            ips = self.shodanner.dns.resolve(args)
+            for i in ips:
+                print(i)
+        else:
+            print("please insert a domain name")
+    
+    #DONE
     def do_honeyscore(self,args):    
         """Calculates a honeypot probability score ranging from 0 (not a honeypot) to 1.0 (is a honeypot)"""
-        print(self.shodanner.honeyscore(args))
+        if args:
+            print(self.shodanner.honeyscore(args))
+        else:
+            print("please insert a valid ip")
 
     def do_clear(self, args):
         """Clear the screen"""
         os.system('clear')
+
+    def do_EOF(self, args):
+        "Close shodanner"""
+        sys.exit()
 
     def do_exit(self, args):
         """Close shodanner"""
